02198f5afb78dbe04455e0226abb8a51e42361bc
agricolamz <agricolamz@gmail.com>
2017-02-28 08:19:55 +0300

    lv distance after selected vowel

--- server.R
+++ server.R
@@ -3,8 +3,8 @@ library(shiny); library(stringr); library(stringdist)
 function(input, output) {
   load("ru_reversed.RData")
   output$view <- renderTable({
-    vowel <- nchar(input$query) - max(grep("[аеёиоуыэюя]", str_sub(tolower(input$query), 1:nchar(input$query), 1:nchar(input$query))))+1
-    ru_reversed[str_sub(ru_reversed$forms, -vowel, -1) == str_sub(input$query, -vowel, -1) &
-                  stringdist(str_sub(ru_reversed$forms, -nchar(input$query), -vowel), str_sub(input$query, -nchar(input$query) -vowel)) <= input$l.dist+1, ]
+    vowel <- nchar(input$query) - grep("[аеёиоуыэюя]", str_sub(tolower(input$query), 1:nchar(input$query), 1:nchar(input$query)))[input$n.vowels]+1
+    ru_reversed[str_sub(ru_reversed$forms, -nchar(input$query), -vowel) == str_sub(input$query, -nchar(input$query), -vowel) &
+                  stringdist(str_sub(ru_reversed$forms, -vowel, -1), str_sub(input$query, -vowel, -1)) <= input$l.dist, ]
       })
 }
--- ui.R
+++ ui.R
@@ -2,9 +2,11 @@ fluidPage(
   titlePanel("Обратный словарь А. А. Зализняка"),
   fluidRow(
     column(3, wellPanel(
-      textInput("query", "", value = "загон"),
-      sliderInput("l.dist", "РЛ перед последним гласным:",
-                  min=0, max=5, value=0),
+      textInput("query", "", value = "паровоз"),
+      sliderInput("l.dist", "РЛ фрагмента после последнего гласного:",
+                  min=0, max=5, value=2),
+      sliderInput("n.vowels", "номер гласного",
+                  min=0, max=10, value=2),
       submitButton("поиск")
     )),
     column(3,
