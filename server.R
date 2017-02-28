e510987bd0173a7699b15dda9c5ed40b493aa47a
agricolamz <agricolamz@gmail.com>
2017-02-28 05:59:05 +0300

    lv distance added

--- server.R
+++ server.R
@@ -1,8 +1,12 @@
-library(shiny); library(stringr)
+library(shiny); library(stringr); library(stringdist)
 
 function(input, output) {
   load("ru_reversed.RData")
   output$view <- renderTable({
-    ru_reversed[str_sub(ru_reversed$forms, -nchar(input$query), -1) == tolower(input$query),]
+    ru_reversed[
+      stringdist(
+        str_sub(ru_reversed$forms, -nchar(input$query), -1),
+        tolower(input$query),
+        method = "lv") <= input$l.dist, ]
   })
 }
--- ui.R
+++ ui.R
@@ -2,7 +2,9 @@ fluidPage(
   titlePanel("Обратный словарь А. А. Зализняка"),
   fluidRow(
     column(2, wellPanel(
-      textInput("query", "", value = "осёл"),
+      textInput("query", "", value = "загон"),
+      sliderInput("l.dist", "Расстояние Левенштейна:",
+                  min=0, max=3, value=0),
       submitButton("поиск")
     )),
     column(3,
