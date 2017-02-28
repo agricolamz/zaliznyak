9eec145b649132bb2231cf74a0a7cc2b049f061d
agricolamz <agricolamz@gmail.com>
2017-02-28 07:17:51 +0300

    lv distance before first vowel

--- server.R
+++ server.R
@@ -3,10 +3,8 @@ library(shiny); library(stringr); library(stringdist)
 function(input, output) {
   load("ru_reversed.RData")
   output$view <- renderTable({
-    ru_reversed[
-      stringdist(
-        str_sub(ru_reversed$forms, -nchar(input$query), -1),
-        tolower(input$query),
-        method = "lv") <= input$l.dist, ]
-  })
+    vowel <- nchar(input$query) - max(grep("[аеёиоуыэюя]", str_sub(tolower(input$query), 1:nchar(input$query), 1:nchar(input$query))))+1
+    ru_reversed[str_sub(ru_reversed$forms, -vowel, -1) == str_sub(input$query, -vowel, -1) &
+                  stringdist(str_sub(ru_reversed$forms, -nchar(input$query), -vowel), str_sub(input$query, -nchar(input$query) -vowel)) <= input$l.dist+1, ]
+      })
 }
--- ui.R
+++ ui.R
@@ -1,10 +1,10 @@
 fluidPage(
   titlePanel("Обратный словарь А. А. Зализняка"),
   fluidRow(
-    column(2, wellPanel(
+    column(4, wellPanel(
       textInput("query", "", value = "загон"),
-      sliderInput("l.dist", "Расстояние Левенштейна:",
-                  min=0, max=3, value=0),
+      sliderInput("l.dist", "РЛ перед последним гласным:",
+                  min=0, max=5, value=0),
       submitButton("поиск")
     )),
     column(3,
