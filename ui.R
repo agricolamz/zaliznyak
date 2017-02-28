a248d8cd097b2a5fb03e28ed2905d069b5576fb3
agricolamz <agricolamz@gmail.com>
2017-02-28 07:26:04 +0300

    lv distance before first vowel

--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 fluidPage(
   titlePanel("Обратный словарь А. А. Зализняка"),
   fluidRow(
-    column(4, wellPanel(
+    column(3, wellPanel(
       textInput("query", "", value = "загон"),
       sliderInput("l.dist", "РЛ перед последним гласным:",
                   min=0, max=5, value=0),
