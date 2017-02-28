bf7c924c3fff00ba1ac3c7af7146cdec3ffb35d8
agricolamz <agricolamz@gmail.com>
2017-02-28 08:34:16 +0300

    lv distance after selected vowel

--- ui.R
+++ ui.R
@@ -3,10 +3,10 @@ fluidPage(
   fluidRow(
     column(3, wellPanel(
       textInput("query", "", value = "паровоз"),
-      sliderInput("l.dist", "РЛ фрагмента после последнего гласного:",
+      sliderInput("l.dist", "РЛ фрагмента после опорного гласного:",
                   min=0, max=5, value=2),
-      sliderInput("n.vowels", "номер гласного",
-                  min=0, max=10, value=2),
+      sliderInput("n.vowels", "номер опорного гласного (с начала)",
+                  min=0, max=10, value=3),
       submitButton("поиск")
     )),
     column(3,
