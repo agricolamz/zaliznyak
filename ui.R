2dee28cf2734b31f63b7f87c30e76faacaeed2a6
agricolamz <agricolamz@gmail.com>
2017-03-01 10:06:15 +0300

    text changed

--- ui.R
+++ ui.R
@@ -1,12 +1,12 @@
 fluidPage(
-  titlePanel("Обратный словарь А. А. Зализняка"),
+  titlePanel("Обратный поиск в словаре А. А. Зализняка"),
   fluidRow(
     column(3, wellPanel(
       textInput("query", "", value = "паровоз"),
       sliderInput("l.dist", "РЛ фрагмента после опорного гласного:",
                   min=0, max=5, value=2),
-      sliderInput("n.vowels", "номер опорного гласного (с начала)",
-                  min=0, max=10, value=3),
+      sliderInput("n.vowels", "номер опорного гласного (с начала слова)",
+                  min=1, max=10, value=3),
       submitButton("поиск")
     )),
     column(3,
