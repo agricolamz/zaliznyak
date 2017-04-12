e703f337b0daa8e65245251bd8e91f02502b9922
agricolamz <agricolamz@gmail.com>
2017-04-12 13:23:11 +0300

    stress added

--- server.R
+++ server.R
@@ -5,6 +5,6 @@ function(input, output) {
   output$view <- renderTable({
     vowel <- nchar(input$query) - grep("[аеёиоуыэюя]", str_sub(tolower(input$query), 1:nchar(input$query), 1:nchar(input$query)))[input$n.vowels]+1
     ru_reversed[str_sub(ru_reversed$forms, -nchar(input$query), -vowel) == str_sub(input$query, -nchar(input$query), -vowel) &
-                  stringdist(str_sub(ru_reversed$forms, -vowel, -1), str_sub(input$query, -vowel, -1)) <= input$l.dist, c(2, 3)]
+                  stringdist(str_sub(ru_reversed$forms, -vowel, -1), str_sub(input$query, -vowel, -1)) <= input$l.dist, ]
       })
 }
