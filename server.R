4667cda4d70b2ba6c990e66719efe8203f21f3ae
agricolamz <agricolamz@gmail.com>
2017-04-12 14:32:31 +0300

    stress slider

--- server.R
+++ server.R
@@ -4,7 +4,8 @@ function(input, output) {
   load("ru_reversed.RData")
   output$view <- renderTable({
     vowel <- nchar(input$query) - grep("[аеёиоуыэюя]", str_sub(tolower(input$query), 1:nchar(input$query), 1:nchar(input$query)))[input$n.vowels]+1
-    ru_reversed[str_sub(ru_reversed$forms, -nchar(input$query), -vowel) == str_sub(input$query, -nchar(input$query), -vowel) &
+    results <- ru_reversed[str_sub(ru_reversed$forms, -nchar(input$query), -vowel) == str_sub(input$query, -nchar(input$query), -vowel) &
                   stringdist(str_sub(ru_reversed$forms, -vowel, -1), str_sub(input$query, -vowel, -1)) <= input$l.dist, ]
+    results[is.na(results$stressed_end)|(results$stressed_end == input$stress) , ]
       })
 }
