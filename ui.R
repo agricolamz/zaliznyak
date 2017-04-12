03a67bbdb10b006f9765ecbf368d0be7d2411512
agricolamz <agricolamz@gmail.com>
2017-04-12 13:06:19 +0300

    stress added

--- ui.R
+++ ui.R
@@ -3,6 +3,8 @@ fluidPage(
   fluidRow(
     column(3, wellPanel(
       textInput("query", "", value = "паровоз"),
+      # sliderInput("stress", "Ударный слог с конца слова:",
+      #             min=1, max=5, value=1),
       sliderInput("l.dist", "РЛ фрагмента после опорного гласного:",
                   min=0, max=5, value=2),
       sliderInput("n.vowels", "номер опорного гласного (с начала слова)",
