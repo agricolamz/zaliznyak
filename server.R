0da4c70922badb2fd7ced802381866fba67c0950
agricolamz <agricolamz@gmail.com>
2017-04-17 07:00:52 +0300

    feather

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
-library(shiny); library(stringr); library(stringdist)
+library(shiny); library(stringr); library(stringdist); library(feather)
 
 function(input, output) {
-  load("ru_reversed.RData")
+  ru_reversed <- read_feather("ru_reversed.feather")
   output$view <- renderTable({
     vowel <- nchar(input$query) - grep("[аеёиоуыэюя]", str_sub(tolower(input$query), 1:nchar(input$query), 1:nchar(input$query)))[input$n.vowels]+1
     results <- ru_reversed[str_sub(ru_reversed$forms, -nchar(input$query), -vowel) == str_sub(input$query, -nchar(input$query), -vowel) &
