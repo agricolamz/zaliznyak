0c8abc7d1578f5961c7f27f4bdb89ded151ae31b
agricolamz <agricolamz@gmail.com>
2017-08-01 13:58:15 +0300

    repair, make faster,

--- server.R
+++ server.R
@@ -3,13 +3,18 @@ library(shiny); library(stringr); library(stringdist); library(feather); library
 function(input, output) {
   ru_reversed <- read_feather("ru_reversed.feather")
   output$fullview <- DT::renderDataTable({
-    ru_reversed[grepl(input$fullquery, ru_reversed$word),]
+    data.frame(result = str_subset(ru_reversed$word, input$fullquery))
   },
   options = list(pageLength = 36, dom = 'ftip'))
   output$view <- DT::renderDataTable({
-    vowel <- nchar(input$query) - grep("[аеёиоуыэюя]", str_sub(tolower(input$query), 1:nchar(input$query), 1:nchar(input$query)))[input$n.vowels]+1
-    results <- ru_reversed[str_sub(ru_reversed$word, -nchar(input$query), -vowel) == str_sub(input$query, -nchar(input$query), -vowel) &
-                  stringdist(str_sub(ru_reversed$word, -vowel, -1), str_sub(input$query, -vowel, -1)) <= input$l.dist, ]
-    results[is.na(results$stressed_s)|(results$stressed_s == input$stress), ]},
+    data <- ru_reversed[ru_reversed$stressed_s == input$stress,]
+    ends <- str_sub(str_replace_all(data$word, "́", ""), -nchar(input$query), -1)
+    result <- data[stringdist(ends, input$query) <= input$l.dist, 1]
+    if(input$n_vowels_before == 0){result} else{
+      result$n_vowels_before <- str_count(result$word, "[аеёиоуыэюя]") - input$stress
+      result[result$n_vowels_before == input$n_vowels_before, 1]
+      }},
     options = list(pageLength = 36, dom = 'ftip'))
 }
+
+
--- ui.R
+++ ui.R
@@ -5,13 +5,13 @@ navbarPage("",
   titlePanel("Обратный поиск в словаре А. А. Зализняка"),
   fluidRow(
     column(3, wellPanel(
-      textInput("query", "", value = "паровоз"),
+      textInput("query", "", value = "нога"),
       sliderInput("stress", "Ударный слог с конца слова:",
                   min=1, max=5, value=1),
       sliderInput("l.dist", "РЛ фрагмента после опорного гласного:",
-                  min=0, max=5, value=2),
-      sliderInput("n.vowels", "номер опорного гласного (с начала слова)",
-                  min=1, max=10, value=2),
+                  min=0, max=5, value=0),
+      sliderInput("n_vowels_before", "Количество слогов перед ударением (не учитывается, если значение — ноль)",
+                  min=0, max=6, value=0),
       submitButton("поиск")
     )),
     column(3,
@@ -22,7 +22,7 @@ tabPanel("Точный поиск",
          titlePanel("Полный поиск по словарю"),
          fluidRow(
           column(3, wellPanel(
-             textInput("fullquery", "", value = "ку")),
+             textInput("fullquery", "", value = "нога")),
              submitButton("поиск")),
          column(3,
                 DT::dataTableOutput("fullview")
