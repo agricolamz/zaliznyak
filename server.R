9c7d201fffb50f7e4a6b7991ad44556ef86e7035
agricolamz <agricolamz@gmail.com>
2017-08-01 19:32:05 +0300

    repair, make faster

--- server.R
+++ server.R
@@ -8,7 +8,12 @@ function(input, output) {
   options = list(pageLength = 36, dom = 'ftip'))
   output$view <- DT::renderDataTable({
     data <- ru_reversed[ru_reversed$stressed_s == input$stress,]
+    stressed_vowel <- str_locate_all(input$query, "[аеёиоуыэюя]")
+    stressed_vowel <- rev(data.frame(stressed_vowel)$start)[input$stress]
+    pattern <- str_sub(input$query, -nchar(input$query), -stressed_vowel-1)
     ends <- str_sub(str_replace_all(data$word, "́", ""), -nchar(input$query), -1)
+    data <- data[str_sub(ends,  -nchar(input$query), -stressed_vowel-1) == pattern,]
+    ends <- ends[str_sub(ends,  -nchar(input$query), -stressed_vowel-1) == pattern]
     result <- data[stringdist(ends, input$query) <= input$l.dist, 1]
     if(input$n_vowels_before == 0){result} else{
       result$n_vowels_before <- str_count(result$word, "[аеёиоуыэюя]") - input$stress
@@ -16,5 +21,3 @@ function(input, output) {
       }},
     options = list(pageLength = 36, dom = 'ftip'))
 }
-
-
